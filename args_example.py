294bb39daf36baa97fb4d8d108330c1e76fa2bce
Edwin Duran <31642430+EdwinDrn@users.noreply.github.com>
2020-07-30 13:00:44 -0400

    Update args_example.py

--- args_example.py
+++ args_example.py
@@ -21,3 +21,14 @@ def user_sum(args):
 
 user_numbers = [2, 2, 2, 2, 2, 3]
 print (user_sum(user_numbers))
+
+
+#The program below uses strings as inputs, then puts them together.
+def user_words(kwargs):
+    results_1 = ""
+    for z in kwargs:
+        results_1 += z
+    return results_1
+
+user_string = ["edwin", "duran"]
+print (user_words(user_string))
